51594548e716b60a9928d82a6856eb10000bfd95
Kevin Denniswhara <wibowokevin425@gmail.com>
2025-08-12 08:07:33 +0700

    [Update] Deleting all unused lib and api

--- src/app/api/posts/delete/route.js
+++
@@ -1,9 +0,0 @@
-import { db } from '@/lib/firebaseConfig'
-import { doc, deleteDoc } from 'firebase/firestore'
-
-export async function POST(req) {
-  const { id } = await req.json()
-  const ref = doc(db, 'posts', id)
-  await deleteDoc(ref)
-  return Response.json({ status: 'deleted' })
-}
